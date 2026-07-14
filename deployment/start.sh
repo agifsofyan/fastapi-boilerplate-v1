8dbae8c87e59a84992f4f324008368fa57915e1a
Agif Sofyan <alpenizme@gmail.com>
2026-07-14 15:19:08 +0700

    refactor start bash

--- deployment/start.sh
+++ deployment/start.sh
@@ -6,4 +6,6 @@ echo "Running migrations..."
 alembic upgrade head
 
 echo "Starting FastAPI..."
-exec uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}
+exec uvicorn app.main:app \
+    --host 0.0.0.0 \
+    --port ${PORT:-8000}
